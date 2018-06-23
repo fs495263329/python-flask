1bd3f29e65551618be7371f53988a8fedb8daef2
fs495263329 <495263329@qq.com>
2018-06-24 01:04:01 +0800

    add all

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #encoding: utf-8
-from  flask  import  Flask,request,render_template,redirect
+from  flask  import  Flask,request,render_template,redirect,url_for,flash
 import  user
 
 app=Flask(__name__)
@@ -30,6 +30,70 @@ def  login():
 @app.route('/users/')
 def users():
     return render_template('users.html',user_list=user.get_users())
+
+@app.route('/user/create/')                         #将url path=/user/create/的get请求交由create_user处理
+def create_user():
+    return render_template('user_create.html')      #加载渲染user_create.html
+
+
+@app.route('/user/add/', methods=['POST'])          #将url path=/user/add的post请求交由add_user处理
+def add_user():
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    age = request.form.get('age', '')
+
+    #检查用户信息
+    _is_ok, _error = user.validate_add_user(username, password, age)
+    if _is_ok:
+        user.add_user(username, password, age)      #检查ok，添加用户信息
+        return redirect(url_for('users', msg='新建成功'))                  #跳转到用户列表url_for
+    else:
+        #跳转到用户新建页面，回显错误信息&用户信息
+        return render_template('user_create.html', \
+                                error=_error, username=username, \
+                                password=password, age=age)
+
+
+@app.route('/user/modify/')                          #将url path=/user/modify/的歌特请求交由modify_user函数处理
+def modify_user():
+    username = request.args.get('username', '')
+    _user = user.get_user(username)
+    _error = ''
+    _username = ''
+    _password = ''
+    _age = ''
+    if _user is None:
+        _error = '用户信息不存在'
+    else:
+        _username = _user.get('username')
+        _password = _user.get('password')
+        _age = _user.get('age')
+
+    return render_template('user_modify.html', error=_error, password=_password, age=_age, username=_username)
+
+@app.route('/user/update/', methods=['POST'])           #将url path=/user/update/的post请求交由update_user函数处理
+def update_user():
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    age = request.form.get('age', '')
+
+    #检查用户信息
+    _is_ok, _error = user.validate_update_user(username, password, age)
+    if _is_ok:
+        user.update_user(username, password, age)
+#        flash('修改用户信息成功')
+        return redirect('/users/')
+    else:
+        return render_template('user_modify.html', error=_error, username=username, password=password, age=age)
+
+@app.route('/user/delete/')
+def delete_user():
+    username = request.args.get('username')
+    user.delete_user(username)
+#    flash('删除用户信息成功')
+    return redirect('/users/')
+
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=9001,debug=True)
--- user.py
+++ user.py
@@ -17,3 +17,79 @@ def validate_login(username,password):
         if  user.get('username')==username and user.get('password')==password:
             return True
     return  False
+
+def validate_add_user(username, password, age):
+    if username.strip() == '':
+        return False, u'用户名不能为空'
+
+    #检查用户名是否重复
+    _users = get_users()
+    for _user in _users:
+        if username == _user.get('username'):
+            return False, u'用户名已存在'
+
+    #密码要求长度必须大于等于6
+    if len(password) < 6:
+        return False, u'密码必须大于等于6'
+
+    if not str(age).isdigit() or int(age) <= 0 or int(age) > 100:
+        return False, u'年龄必须是0到100的数字'
+
+    return True, ''
+
+
+def add_user(username, password, age):
+    _user = {'username' : username, 'password' : password, 'age' : age}
+    _users = get_users()
+    _users.append(_user)
+    save_users(_users)
+
+def save_users(users):
+    fhandler = open(gconf.USER_FILE, 'wb')
+    fhandler.write(json.dumps(users))
+    fhandler.close()
+
+def get_user(username):
+    _users = get_users()
+    for _user in _users:
+        if _user.get('username') == username:
+            return _user
+
+    return None
+
+def validate_update_user(username, password, age):
+    if get_user(username) is None:
+        return False, u'用户信息不存在'
+
+    #密码要求长度必须大于等于6
+    if len(password) < 6:
+        return False, u'密码必须大于等于6'
+
+    if not str(age).isdigit() or int(age) <= 0 or int(age) > 100:
+        return False, u'年龄必须是0到100的数字'
+
+    return True, ''
+
+
+
+
+def update_user(username, password, age):
+    _users = get_users()
+    for _user in _users:
+        if _user.get('username') == username:
+            _user['password'] = password
+            _user['age'] = age
+            save_users(_users)
+            break
+
+
+def delete_user(username):
+    _users = get_users()
+    _idx = -1
+    for _user in _users:
+        _idx += 1
+        if _user.get('username') == username:
+            del _users[_idx]
+            save_users(_users)
+            break
+
