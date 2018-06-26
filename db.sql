853e3924611570104cb9764da4bf8ce94522f3d0
fs495263329 <495263329@qq.com>
2018-06-27 00:52:22 +0800

    add add

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user2 (
+  id int(11) NOT NULL AUTO_INCREMENT primary  key,
+  username varchar(25) DEFAULT NULL,
+  password varchar(32) DEFAULT NULL,
+  age int(11) DEFAULT NULL
+) ENGINE=InnoDB   CHARSET=utf8; 
+
